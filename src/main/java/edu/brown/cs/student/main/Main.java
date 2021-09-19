05a1721d130127884b502f2e02a99e18d8b28ab9
szg9zsmmj6ldnr2i <90581999+szg9zsmmj6ldnr2i@users.noreply.github.com>
2021-09-19 14:44:19 -0400

    Update Main.java

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -1,12 +1,7 @@
 package edu.brown.cs.student.main;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 import com.google.common.collect.ImmutableMap;
 
@@ -28,6 +23,22 @@ public final class Main {
 
   // use port 4567 by default when running server
   private static final int DEFAULT_PORT = 4567;
+
+  class Star {
+    int StarID;
+    String ProperName;
+    double X;
+    double Y;
+    double Z;
+
+    public Star(int starID, String properName, double x, double y, double z) {
+      StarID = starID;
+      ProperName = properName;
+      X = x;
+      Y = y;
+      Z = z;
+    }
+  }
 
   /**
    * The initial method called when execution begins.
@@ -53,13 +64,13 @@ public final class Main {
 
     // use "--port <n>" to specify what port on which the server runs
     parser.accepts("port").withRequiredArg().ofType(Integer.class)
-        .defaultsTo(DEFAULT_PORT);
+            .defaultsTo(DEFAULT_PORT);
 
     OptionSet options = parser.parse(args);
     if (options.has("gui")) {
       runSparkServer((int) options.valueOf("port"));
     }
-
+    HashMap<String, Star> hMap = new HashMap<String, Star>();
     // TODO: Add your REPL here!
     try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
       String input;
@@ -68,26 +79,61 @@ public final class Main {
           input = input.trim();
           String[] arguments = input.split(" ");
 //          System.out.println(arguments[0]);
+
           if (arguments[0].equals("add")) {
-            MathBot mathbot= new MathBot();
+            MathBot mathbot = new MathBot();
             double result = mathbot.add(Double.valueOf(arguments[1]), Double.valueOf(arguments[2]));
             System.out.println(result);
-          }
-          else if (arguments[0].equals("subtract")) {
-            MathBot mathbot= new MathBot();
-            double result = mathbot.subtract(Double.valueOf(arguments[1]),Double.valueOf(arguments[2]));
+          } else if (arguments[0].equals("subtract")) {
+            MathBot mathbot = new MathBot();
+            double result = mathbot.subtract(Double.valueOf(arguments[1]), Double.valueOf(arguments[2]));
             System.out.println(result);
-          }
-          else if  (arguments[0].equals("stars")) {
-            FileReader fr=new FileReader(arguments[1]);
-            BufferedReader br=new BufferedReader(fr);
-
+          } else if (arguments[0].equals("stars")) {
+            FileInputStream fis = new FileInputStream(arguments[1]);
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(fis));
             String line = null;
-            while ((line = br.readLine()) != null) {
-              System.out.println(line);
+            int count = 0;
+            while ((line = br2.readLine()) != null) {
+              if (count == 0) {
+                count++;
+                continue;
+              }
+              String[] datas = line.split(",");
+              hMap.put(datas[1], new Star(Integer.parseInt(datas[0]), datas[1], Double.parseDouble(datas[2]), Double.parseDouble(datas[3]), Double.parseDouble(datas[4])));
+              count++;
             }
+            System.out.println("Read " + (count - 1) + " stars from " + arguments[1]);
             br.close();
-            fr.close();
+          } else if (arguments[0].equals("naive_neighbors")) {
+            int n = Integer.parseInt(arguments[1]);
+            if (n == 0) continue;
+            List<Map.Entry<Integer, Double>> ids = new ArrayList<Map.Entry<Integer, Double>>(distances.entrySet());
+            if (arguments.length == 3) {
+              Star star = hMap.get(arguments[2]);
+              HashMap<Integer, Double> distances = new HashMap<Integer, Double>();
+              for (Star i : hMap.values()) {
+                double d = (i.X - star.X) * (i.X - star.X) + (i.Y - star.Y) * (i.Y - star.Y) + (i.Z - star.Z) * (i.Z - star.Z);
+                distances.put(i.StarID, d);
+              }
+            } else {
+              HashMap<Integer, Double> distances = new HashMap<Integer, Double>();
+              double X = Double.parseDouble(arguments[2]);
+              double Y = Double.parseDouble(arguments[3]);
+              double Z = Double.parseDouble(arguments[4]);
+              for (Star i : hMap.values()) {
+                double d = (i.X - X) * (i.X - X) + (i.Y - Y) * (i.Y - Y) + (i.Z - Z) * (i.Z - Z);
+                distances.put(i.StarID, d);
+              }
+            }
+            ids.sort(new Comparator<Map.Entry<Integer, Double>>() {
+              @Override
+              public int compare(Map.Entry<Integer, Double> o1, Map.Entry<Integer, Double> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+              }
+            });
+            for (int i = 0; i < n; i++)
+              System.out.println(ids.get(i).getKey());
+
           }
 
           // TODO: complete your REPL by adding commands for addition "add" and subtraction
@@ -113,7 +159,7 @@ public final class Main {
       config.setDirectoryForTemplateLoading(templates);
     } catch (IOException ioe) {
       System.out.printf("ERROR: Unable use %s for template loading.%n",
-          templates);
+              templates);
       System.exit(1);
     }
     return new FreeMarkerEngine(config);
@@ -167,7 +213,7 @@ public final class Main {
     public ModelAndView handle(Request req, Response res) {
       // this is a map of variables that are used in the FreeMarker template
       Map<String, Object> variables = ImmutableMap.of("title",
-          "Go go GUI");
+              "Go go GUI");
 
       return new ModelAndView(variables, "main.ftl");
     }
